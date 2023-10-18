29c106622940a89c352f635559f931f60d4bc008
proudnhello <92887967+proudnhello@users.noreply.github.com>
2023-10-17 17:47:53 -0700

    clock and reset work

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -44,14 +44,45 @@ class Play extends Phaser.Scene{
             frames: this.anims.generateFrameNumbers('explosion', {start :0, end: 9, first: 0}),
             frameRate:30
         })
+
+        this.p1Score = 0;
+        let scoreConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 100
         }
+        this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
+
+        // Timer fun
+        this.gameOver = false;
+
+        scoreConfig.fixedWidth = 0;
+        this.clock = this.time.delayedCall(60 * 1000, () => {
+                this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+                this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+                this.gameOver = true;
+        }, null, this);
+    }
 
     update() {
         this.starfield.tilePositionX -= 2;
-        this.p1Rocket.update();
-        this.ship01.update();
-        this.ship02.update();
-        this.ship03.update();
+        if(!this.gameOver){
+            this.p1Rocket.update();
+            this.ship01.update();
+            this.ship02.update();
+            this.ship03.update();
+        }
+
+        if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)){
+            this.scene.restart();
+        }
 
         // check collisions
         if(this.checkCollision(this.p1Rocket, this.ship03)) {
@@ -87,5 +118,7 @@ class Play extends Phaser.Scene{
             ship.alpha = 1;
             boom.destroy();
         })
+        this.p1Score += ship.points;
+        this.scoreLeft.text = this.p1Score;
       }
 }
